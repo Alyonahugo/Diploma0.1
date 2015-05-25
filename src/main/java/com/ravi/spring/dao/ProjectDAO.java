4457ff8fdc7c7f123c58b34e9453a4261ac4ad5f
AlyonaHugo <AlyonaHugo@gmail.com>
2015-05-25 13:12:40 +0300

    add unexisting elements

---
+++ src/main/java/com/ravi/spring/dao/ProjectDAO.java
@@ -0,0 +1,29 @@
+package com.ravi.spring.dao;
+
+
+import com.ravi.enumaration.Status;
+import com.ravi.spring.model.Project;
+
+import java.util.List;
+
+/**
+ * Created by User on 18.05.2015.
+ */
+public interface ProjectDAO {
+
+    public void addProject(Project project);
+
+
+    public void deleteProject(Project project);
+
+
+    public void updateProject(Project project);
+
+
+    public Project getProjectById(int id);
+
+
+    public List<Project> getProjects();
+
+    public List<Project> getProjectsByStatus(Status status);
+}
---
+++ src/main/java/com/ravi/spring/dao/impl/ProjectDAOImpl.java
@@ -0,0 +1,64 @@
+package com.ravi.spring.dao.impl;
+
+
+import com.ravi.enumaration.Status;
+import com.ravi.spring.dao.ProjectDAO;
+import com.ravi.spring.model.Project;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by User on 18.05.2015.
+ */
+
+@Repository
+public class ProjectDAOImpl implements ProjectDAO {
+
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void addProject(Project project) {
+        sessionFactory.getCurrentSession().save(project);
+    }
+
+    @Override
+    public void deleteProject(Project project) {
+
+    }
+
+    @Override
+    public void updateProject(Project project) {
+        getSessionFactory().getCurrentSession().update(project);
+    }
+
+    @Override
+    public Project getProjectById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjects() {
+        return sessionFactory.getCurrentSession().createQuery("from Project").list();
+    }
+
+    @Override
+    public List<Project> getProjectsByStatus(Status status) {
+        List<Project> list = getSessionFactory().getCurrentSession()
+                .createQuery("from Project  where status=?")
+                .setParameter(0, status).list();
+        return list;
+    }
+}
