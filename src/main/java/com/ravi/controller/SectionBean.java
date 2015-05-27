f84e0d7a602da402b8bff1c181aab47186a7bb25
AlyonaHugo <AlyonaHugo@gmail.com>
2015-05-27 20:18:22 +0300

    update section page with redirect into topics page

--- src/main/java/com/ravi/controller/SectionBean.java
+++ src/main/java/com/ravi/controller/SectionBean.java
@@ -3,11 +3,17 @@ package com.ravi.controller;
 import com.ravi.spring.model.Section;
 import com.ravi.spring.service.CommentService;
 import com.ravi.spring.service.SectionService;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -21,24 +27,42 @@ public class SectionBean implements Serializable {
     private List<Section> sections;
     private String secName;
 
+@Autowired
+    private Section selectedSection;
+
+
     @Autowired
     @ManagedProperty(value="#{sectionService}")
     private SectionService sectionService;
+
+
+
 
   //  @PostConstruct
     public  void init(){
         sections = sectionService.getSections();
     }
 
-    public void addSection(){
+    public void addSection() throws IOException {
         Section section = new Section();
         section.setName(secName);
         sectionService.addSection(section);
         System.out.println("method addsection work");
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect(ec.getRequestContextPath() + "/pages/vote.xhtml");
+    //    return "vote.xhtml";
     }
     public List<Section> getSections() {
         init();
         return sections;
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        System.out.println("method work - onRowSelect");
+    }
+
+    public void onRowUnselect(UnselectEvent event) {
+        System.out.println("onRowUnselect have worked");
     }
 
     public void setSections(List<Section> sections) {
@@ -59,5 +83,22 @@ public class SectionBean implements Serializable {
 
     public void setSecName(String secName) {
         this.secName = secName;
+    }
+
+    public Section getSelectedSection() {
+        return selectedSection;
+    }
+
+    public void setSelectedSection(Section selectedSection) {
+        this.selectedSection = selectedSection;
+
+
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            ec.redirect(ec.getRequestContextPath() + "/pages/topic.xhtml");
+        } catch (IOException e) {
+
+        }
+
     }
 }
